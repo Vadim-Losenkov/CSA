12d70cf63748071008144747e8b945b7c278b0f2
Vadim Losenkov <vprog0729@gmail.com>
2022-02-09 10:34:20 +0300

    slider working

--- app/js/slider.js
+++ app/js/slider.js
@@ -2,42 +2,80 @@ class Slider {
   constructor(settings = {}) {
     this.settings = settings
     this.$wrapper = document.querySelector(settings.wrapper)
-    
+    this.$slider = this.$wrapper.querySelector(this.settings.slider)
+
     this.init()
   }
   
   init() {
-    const $slides = this.$wrapper.querySelectorAll(this.settings.item)
+    this.getSlidesPosition
+    this.routerListener()
+
+    this.setClasses(this.$slides)
+  }
+
+  get getSlidesPosition() {
+    this.$slides = this.$wrapper.querySelectorAll(this.settings.item)
     const windowWidth = window.innerWidth
-    $slides.forEach($item => {
+    
+    this.$slides.forEach($item => {
+      $item.classList.remove('left-shadow')
+      $item.classList.remove('right-shadow')
+    })
+    this.$slides.forEach($item => {
       const rectX = Math.round($item.getBoundingClientRect().x)
-      if (rectX < 0) {
+      console.log(rectX);
+      if (rectX < 40) {
         $item.classList.add('left-shadow')
       } else if (windowWidth - (rectX + $item.getBoundingClientRect().width) < 0) {
         $item.classList.add('right-shadow')
-      } else {
-        $item.classList.add('visible')
       }
     })
   }
-  /*
-     all-child translate
-     last-child remove sett
-     first-child add sett with trandlate than animation
-  */
-  get checkSliderParams() {
-    const $slider = this.$wrapper.querySelector(this.settings.slider)
-    const $items = $slider.querySelectorAll(this.settings.item)
-    
-    return {
-      width: $slider.offsetWidth,
-      rect: $slider.getBoundingClientRect(),
-      $slider, $items
-    }
-  }
   
   routerListener() {
-    
+    const $routerWrapper = this.$wrapper.querySelector(this.settings.routerWrapper)
+
+    $routerWrapper.addEventListener('click', (event) => {
+      const $target = event.target
+      const buttonValue = $target.closest('[data-slider-button]').dataset.sliderButton
+
+      if (buttonValue === 'left') {
+        this.changeSlidesLeft
+      } else if (buttonValue === 'right') {
+        this.changeSlidesRight
+      }
+    })
+  }
+
+  get changeSlidesRight() {
+    const $lastSlide = this.$slides[this.$slides.length-1]
+    this.$slider.insertAdjacentElement('afterbegin', $lastSlide)
+    this.getSlidesPosition
+
+    this.setClasses(this.$slides)
+  }
+  
+  get changeSlidesLeft() {
+    const $lastSlide = this.$slides[0]
+    this.$slider.insertAdjacentElement('beforeend', $lastSlide)
+    this.getSlidesPosition
+
+    this.setClasses(this.$slides)
+  }
+
+  setClasses($elArray) {
+    $elArray.forEach(($el, idx) => {
+      let className
+      $el.classList.forEach(cl => {
+        if (cl.startsWith('item-')) {
+          className =  cl
+        }
+      })
+
+      $el.classList.remove(className)
+      $el.classList.add(`item-${idx + 1}`)
+    })
   }
 }
 
@@ -45,5 +83,30 @@ new Slider({
   wrapper: '[data-slider="wrapper"]',
   slider: '[data-slider="slider"]',
   item: '.roadmap__item',
-  buttonsWrapper: ''
+  routerWrapper: '[data-slider="router"]',
+  seasons: {
+    elementSelector: '[data-slider-item]',
+    objArray: [
+      {
+        name: 'Зима',
+        year: 2021,
+        elements: [0, 1, 2]
+      },
+      {
+        name: 'Весна',
+        year: 2021,
+        elements: [0, 1, 2]
+      },
+      {
+        name: 'Лето',
+        year: 2021,
+        elements: [0, 1, 2]
+      },
+      {
+        name: 'Осень',
+        year: 2021,
+        elements: [0, 1, 2]
+      },
+    ]
+  }
 })
