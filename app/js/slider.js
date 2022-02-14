31f5ae50be61d69a769bb412a74431337454e684
Vadim Losenkov <vprog0729@gmail.com>
2022-02-14 14:33:29 +0300

    added mobile roadmap

--- app/js/slider.js
+++ app/js/slider.js
@@ -24,7 +24,7 @@ class Slider {
     })
     this.$slides.forEach($item => {
       const rectX = Math.round($item.getBoundingClientRect().x)
-      console.log(rectX);
+
       if (rectX < 40) {
         $item.classList.add('left-shadow')
       } else if (windowWidth - (rectX + $item.getBoundingClientRect().width) < 0) {
@@ -34,38 +34,7 @@ class Slider {
   }
 
   routerListener() {
-    this.$routerWrapper = this.$wrapper.querySelector(this.settings.routerWrapper)
-
-    this.$routerWrapper.addEventListener('click', (event) => {
-      const $target = event.target
-      const buttonValue = $target.closest('[data-slider-button]').dataset.sliderButton
-
-      if (buttonValue === 'left') {
-        this.changeSlidesLeft
-      } else if (buttonValue === 'right') {
-        this.changeSlidesRight
-      }
-    })
-
-    this.renderSeason()
-  }
-
-  get changeSlidesRight() {
-    const $lastSlide = this.$slides[this.$slides.length - 1]
-    this.$slider.insertAdjacentElement('afterbegin', $lastSlide)
-    this.getSlidesPosition
-
-    this.setClasses(this.$slides)
-    this.setSeason()
-  }
-
-  get changeSlidesLeft() {
-    const $lastSlide = this.$slides[0]
-    this.$slider.insertAdjacentElement('beforeend', $lastSlide)
-    this.getSlidesPosition
-
-    this.setClasses(this.$slides)
-    this.setSeason()
+    
   }
 
   setClasses($elArray) {
@@ -82,76 +51,31 @@ class Slider {
     })
   }
 
-  renderSeason() {
-    this.$seasonWrapper = this.$routerWrapper.querySelector('[data-slider="season"]')
-    const seasonHTML = this.settings.seasons.objArray.map((obj, idx) => `
-      <div class="roadmap__router-info" data-slider-season="${idx}">
-        <div class="roadmap__router-season">
-          ${obj.name}
-        </div>
-        <div class="roadmap__router-year">
-          ${obj.year}
-        </div>
-      </div>
-    `).join('')
-    this.$seasonWrapper.insertAdjacentHTML('beforeend', seasonHTML)
-    this.setSeason()
-  }
-
-  setSeason() {
-    const $el = this.$wrapper.querySelector('.item-4')
-
-    if ($el) {
-      this.settings.seasons.objArray.forEach((obj, idx) => {
-        if (obj.elements.includes(+$el.dataset.sliderItem)) {
-          this.$seasonWrapper.querySelectorAll('.roadmap__router-info').forEach($el => {
-            $el.style.transform = `translateX(-${100 * idx}px)`
-            $el.style.transition = `all .3s`
-          })
-        }
-      })
-    }
-  }
-
   desrtoy() {
     // написать метод destroy
   }
 }
-
-const routerIdx = [
-  {
-    name: 'Зима',
-    year: 2021,
-    elements: [0, 1]
-  },
-  {
-    name: 'Весна',
-    year: 2021,
-    elements: [2, 3]
-  },
-  {
-    name: 'Лето',
-    year: 2021,
-    elements: [4]
-  },
-  {
-    name: 'Осень',
-    year: 2021,
-    elements: [5]
-  },
-]
 
 const seasonItems = document.querySelectorAll('[data-slider-season]')
 seasonItems.forEach(($el, idx) => {
   $el.setAttribute('data-router-id', idx)
 })
 
-function setRouter(index) {
+function setRouter(index, defaultPosition) {
+  if (defaultPosition) {
+    seasonItems.forEach($el => setDefaultRouter($el, defaultPosition))
+  }
   seasonItems.forEach(($el, idx) => {
     if (JSON.parse($el.dataset.sliderSeason).includes(index)) {
-      console.log($el);
+      seasonItems.forEach($el => {
+        setDefaultRouter($el, idx)
+      })
     }
   })
+  function setDefaultRouter($el, i) {
+    $el.style.transform = `translateX(-${100 * i}px)`
+    $el.style.transition = `all .3s`
+  }
 }
 
 const breakpoint = window.matchMedia('(max-width: 768px)')
@@ -159,12 +83,12 @@ const breakpoint = window.matchMedia('(max-width: 768px)')
 let customSlider 
 let mySwiper
 
-const breakpointChecker = function () {
+const breakpointChecker = () => {
   if (!breakpoint.matches) {
     if (mySwiper !== undefined) {
       mySwiper.destroy(true, true)
-      enableCustomSlider()
     }
+    enableCustomSlider()
   } else if (breakpoint.matches) {
     if (customSlider !== undefined) {
       customSlider.desrtoy()
@@ -180,6 +104,11 @@ const enableSwiper = () => {
     centeredSlides: true,
     slidesPerView: "auto",
     spaceBetween: 20,
+    breakpoints: {
+      440: {
+        spaceBetween: -10,
+      }
+    },
     navigation: {
       nextEl: '.button-next',
       prevEl: '.button-prev',
@@ -189,7 +118,7 @@ const enableSwiper = () => {
       stretch: 0,
       depth: 100,
       modifier: 5,
-      slideShadows: true,
+      slideShadows: false,
     },
   })
 
@@ -208,28 +137,6 @@ const enableCustomSlider = () => {
     routerWrapper: '[data-slider="router"]',
     seasons: {
       elementSelector: '[data-slider-item]',
-      objArray: [
-        {
-          name: 'Зима',
-          year: 2021,
-          elements: [0, 1]
-        },
-        {
-          name: 'Весна',
-          year: 2021,
-          elements: [2, 3]
-        },
-        {
-          name: 'Лето',
-          year: 2021,
-          elements: [4]
-        },
-        {
-          name: 'Осень',
-          year: 2021,
-          elements: [5]
-        },
-      ]
     }
   })
 }
